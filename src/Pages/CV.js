34e0e92e6c8b4a243136e5a280e108e6b1215182
Christine Hoang <christinehoangt@gmail.com>
2025-01-04 16:24:03 -0600

    trying iframe for cv

--- src/Pages/CV.js
+++ src/Pages/CV.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { cvDetails } from '../Details';
 
 const CV = () => {
     return (
         <div className="flex items-center justify-center h-screen w-screen">
-            <embed
-                src={cvDetails.cvUrl}
-                type="application/pdf"
-                className="border-none w-full h-full max-w-4xl max-h-screen"
-                scrolling="auto"
-            />
+            <iframe 
+                src="https://docs.google.com/document/d/e/2PACX-1vSk3gaH4mUdaL6bgw4IFBWoUbyetsyGKxh02yw0NFJpSwKFLVz7vP7-euH8Qt6V3feCjjjfHLkrYvxM/pub?embedded=true"
+                className="w-full h-full max-w-4xl max-h-screen border-0"
+                title="CV Document"
+                allowFullScreen
+            ></iframe>
         </div>
     );
 };
